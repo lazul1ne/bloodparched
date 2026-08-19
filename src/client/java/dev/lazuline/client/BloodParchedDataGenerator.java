de2637d1213eb0a58ce40408ec5f9bc37d8b7903
lazul1ne <78873973+lazul1ne@users.noreply.github.com>
2026-08-18 22:19:51 -0400

    upload template

---
+++ src/client/java/dev/lazuline/client/BloodParchedDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.lazuline.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BloodParchedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
